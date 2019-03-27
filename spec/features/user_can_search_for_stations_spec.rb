08f569e34a663c0ab82e1538ecf85fb7c4e48ff9
Julia Marco <marcootj@gmail.com>
2019-03-27 09:40:44 -0600

    Added search controller and its index view and started BDDing the view

--- spec/features/user_can_search_for_stations_spec.rb
+++ spec/features/user_can_search_for_stations_spec.rb
@@ -25,12 +25,6 @@ describe 'as a User' do
   end
 end
 
-# As a user
-# When I visit "/"
-# And I fill in the search form with 80206 (Note: Use the existing search form)
-# And I click "Locate"
-# Then I should be on page "/search"
-# Then I should see the total results of the stations that match my query.
 # Then I should see a list of the 15 closest stations within 5 miles sorted by distance
 # And the stations should be limited to Electric and Propane
 # And the stations should only be public, and not private, planned or temporarily unavailable.
